77b06121e9224dd014a49bba6e05c157f38aec8b
Andrey Martynov <d.g.dev482@gmail.com>
2019-07-01 16:44:16 +0500

    Добавлены миграции для свойств моделей

---
+++ migrations/2019_07_01_103042_create_system_properties.php
@@ -0,0 +1,44 @@
+<?php
+
+use FastDog\Core\Properties\BaseProperties;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemProperties extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string(BaseProperties::NAME, 50)->nullable();
+            $table->string(BaseProperties::ALIAS, 50)->nullable();
+            $table->string(BaseProperties::TYPE, 10)->nullable();
+            $table->integer(BaseProperties::MODEL)->nullable()->comment('HEX строки название модели переведенный в INT');
+            $table->integer(BaseProperties::SORT)->default(100);
+            $table->string(BaseProperties::VALUE)->nullable();
+            $table->json(BaseProperties::DATA)->comment('Дополнительные параметры');
+
+            $table->index(BaseProperties::MODEL, 'IDX_system_properties_model');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_properties');
+    }
+}
---
+++ migrations/2019_07_01_103954_create_system_properties_select_values.php
@@ -0,0 +1,49 @@
+<?php
+
+use FastDog\Core\Properties\BasePropertiesSelectValues;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreteSystemPropertiesSelectValues extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_properties_select_values', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer(BasePropertiesSelectValues::PROPERTY_ID);
+            $table->string(BasePropertiesSelectValues::NAME, 50);
+            $table->string(BasePropertiesSelectValues::ALIAS, 50);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::table('system_properties_select_values', function ($table) {
+            $table->foreign(BasePropertiesSelectValues::PROPERTY_ID, 'FK_system_properties')
+                ->references('id')
+                ->on('system_properties');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('system_properties_select_values')) {
+            Schema::table('system_properties_select_values', function ($table) {
+                $table->dropForeign('FK_system_properties');
+            });
+        }
+
+        Schema::dropIfExists('system_properties_select_values');
+    }
+}
