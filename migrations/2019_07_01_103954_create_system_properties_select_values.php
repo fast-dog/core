292d52f8288a7fdca3b182ff50b518548a19379a
Andrey Martynov <d.g.dev482@gmail.com>
2019-07-01 17:55:26 +0500

    fix migration

--- migrations/2019_07_01_103954_create_system_properties_select_values.php
+++ migrations/2019_07_01_103954_create_system_properties_select_values.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreteSystemPropertiesSelectValues extends Migration
+class CreateSystemPropertiesSelectValues extends Migration
 {
     /**
      * Run the migrations.
