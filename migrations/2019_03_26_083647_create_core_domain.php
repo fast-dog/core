8ec3be600040d0be7fa787b30f01ed2abcf6eddf
dg <d.g.dev482@gmail.com>
2019-07-27 16:18:37 +0500

    fix migration

--- migrations/2019_03_26_083647_create_core_domain.php
+++ migrations/2019_03_26_083647_create_core_domain.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use FastDog\Core\Models\Domain;
 
 class CreateCoreDomain extends Migration
 {
@@ -15,7 +16,7 @@ class CreateCoreDomain extends Migration
     public function up()
     {
         if (!Schema::hasTable('core_domain')) {
-            Schema::create('core_domain', function (Blueprint $table) {
+            Schema::create('core_domain', function(Blueprint $table) {
                 $table->increments('id');
                 $table->string('name')->comment('Название');
                 $table->string('url')->comment('HTTP адрес домена в формате http://{name}.{com}');
@@ -28,6 +29,16 @@ class CreateCoreDomain extends Migration
                 $table->softDeletes();
             });
             DB::statement("ALTER TABLE `core_domain` comment 'Доступные домены'");
+
+            Domain::create([
+                Domain::NAME => 'Сайт по умолчанию',
+                Domain::CODE => '001',
+                Domain::SITE_ID => '001',
+                Domain::STATE => Domain::STATE_PUBLISHED,
+                Domain::URL => '/',
+                Domain::LANG => config('app.locale'),
+                Domain::DATA => json_encode([])
+            ]);
         }
     }
 
