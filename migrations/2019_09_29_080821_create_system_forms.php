ef6a99be96a0a1da2c7302f1ca1f945be917a6fe
dg <d.g.dev482@gmail.com>
2019-09-29 14:36:27 +0500

    dev forms, add migration

---
+++ migrations/2019_09_29_080821_create_system_forms.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSystemForms extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('system_forms')) {
+            Schema::create('system_forms', function(Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->integer('model')->nullable()->comment('HEX строки название модели переведенный в INT');
+                $table->integer('user_id')->nullable()->comment('Идентификатор пользователя');
+                $table->json('data')->comment('Дополнительные параметры');
+                $table->timestamps();
+            });
+            DB::statement("ALTER TABLE `system_forms` comment 'Формы в разделе администрирования'");
+        }
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_forms');
+    }
+}
--- src/Form/BaseForm.php
+++ src/Form/BaseForm.php
@@ -2,6 +2,8 @@
 
 namespace FastDog\Core\Form;
 
+use FastDog\Core\Models\BaseModel;
+
 /**
  * Формы раздела администрирования
  *
@@ -9,7 +11,27 @@ namespace FastDog\Core\Form;
  * @version 0.2.0
  * @author Андрей Мартынов <d.g.dev482@gmail.com>
  */
-class BaseForm
+class BaseForm extends BaseModel
 {
+    /**
+     * Модель к которой принадлежит параметр
+     * @cont string
+     */
+    const MODEL = 'model';
 
+    /**
+     * Идентификатор пользователя
+     * cont string
+     */
+    const USER_ID = 'user_id';
+
+    /**
+     * @var string $table
+     */
+    public $table = 'system_forms';
+
+    /**
+     * @var array $fillable
+     */
+    public $fillable = [self::NAME, self::MODEL, self::USER_ID, self::DATA];
 }
